f8dc59e66e472c24215e3875ae6911292b34775a
Dowlet20 <dovikgandy@gmail.com>
2025-02-18 16:19:46 +0500

    latest versionn

--- utils/axiosInstance.tsx
+++ utils/axiosInstance.tsx
@@ -1,19 +1,19 @@
 import axios, { AxiosInstance} from 'axios';
 
-export const base_URL = 'http://10.10.73.49:8000';
+export const base_URL = 'http://10.20.7.42:5000';
 
 interface MyResponseType {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: base_URL,
-  timeout: 10000, 
-  headers: {
-    'Content-Type': 'application/json',
-  }
+	baseURL: base_URL,
+	timeout: 10000, 
+	headers: {
+		'Content-Type': 'application/json',
+	}
 });
 
 export default axiosInstance;
